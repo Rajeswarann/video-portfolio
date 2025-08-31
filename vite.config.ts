f62fdc1346e3ab86e63e3a3fefcf4fbd9215021f
Aishudivya0620 <aishudivya0620@gmail.com>
2025-08-31 10:37:02 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/video-editor-portfolio/', // 👈 This is your repo name
+  base: '/video-portfolio/', // 👈 This is your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
